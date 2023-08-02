a636647a3af1732361277939fe17e721ea8eadc5
AStudentProgrammer <linzhi263754@gmail.com>
2023-08-02 17:30:30 +0800

    Storing repos
    Basic-Augmented contains my code
    MAVSDK-Python Contains my code
    rpi_cam_cali

---
+++ rpi_cam_cali/webcam.py
@@ -0,0 +1,41 @@
+# import the opencv library
+import cv2
+
+from time import sleep,time
+prev_time = 0
+
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+count = 0
+
+
+while(True):
+    current_time = time()
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    filename = "img_" + str(count) + ".jpeg"
+    
+    if current_time - prev_time >= 10:
+        cv2.imwrite(filename,frame)
+        prev_time = time()        
+        count = count + 1
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+    #sleep(10)
+        
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
